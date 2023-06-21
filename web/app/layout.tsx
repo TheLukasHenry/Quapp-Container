ac7abebf38a719029127ab0eef9b96b526ee3187
‘lukasHerajt’ <lherajt@gmail.com>
2023-06-21 17:48:22 +0000

    tailwind and font working clean

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,5 @@
 import AuthProvider from './AuthProvider'
 import NavMenu from '../components-client/NavMenu'
-import Hero from '@/components-client/Hero'
 import Footer from '@/components-client/Footer'
 import '@/styles/globals.css'
 import { Poppins } from 'next/font/google'
@@ -24,13 +23,9 @@ export default function RootLayout({
     <AuthProvider>
       <html lang="en">
         <body className={myFont.className}>
-          <p></p>
-          <h1 className="text-pink-500 text-6xl mb-0 underline">QuappLayerr</h1>
-
           <NavMenu />
-          <Hero />
-          <Footer />
           <div>{children} </div>
+          <Footer />
         </body>
       </html>
     </AuthProvider>
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from '@/components-client/Hero'
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+    <div className="flex min-h-screen flex-col items-center justify-between">
+      <Hero />
     </div>
   )
 }
