c0ef96ed42f6f70a75f2bdc6e610eb39eeb1182d
Ross Gordon <rossagordonstl@gmail.com>
2023-07-06 02:51:03 +0000

    updated CSS to tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: '#00FF00',
+        tertiary: '#0000FF',
+      },
+    },
   },
   plugins: [],
 }
